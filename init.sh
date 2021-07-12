a3c9f4b62ff3f8bc80a106fb2235a2f26825d5ac
aymfx <1095731371@qq.com>
2021-07-12 14:58:48 +0800

    更新

---
+++ init.sh
@@ -0,0 +1,7 @@
+echo "# sampleCode" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:aymfx/sampleCode.git
+git push -u origin main
